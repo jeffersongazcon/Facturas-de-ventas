b9245fac44e930bc48b80e817a71122da885f443
1Daniel-cogollo <dandrescogollo@unicesar.edu.co>
2024-05-13 20:17:06 -0500

    Icrud creado capa de datos

--- DAL/DetalleFacturaRepositorio.cs
+++ DAL/DetalleFacturaRepositorio.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ENTITY;
 
 namespace DAL
 {
-    public class Class1
+    public class DetalleFacturaRepositorio : IcrudBase<DetalleFactura>
     {
+        
     }
 }
